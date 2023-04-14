6e3c560922d24e37ff6a31f9467699f949da53d6
Felizardo Massimbe <felizardo.n.massimbe@aubay.pt>
2023-04-14 20:55:43 +0100

    feat: create answer component

---
+++ src/components/PollAnswer.js
@@ -0,0 +1,10 @@
+const PollAnswer = () => {
+  return (
+    <div className="answer">
+      <p className="answer__text">Build our new application with JS</p>
+      <button className="answer__button">Click</button>
+    </div>
+  );
+};
+
+export default PollAnswer;
