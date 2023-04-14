b41cb726ef70521a6df47e275b96cae9039f0532
Felizardo Massimbe <felizardo.n.massimbe@aubay.pt>
2023-04-14 22:18:52 +0100

    feat: create login page layout

---
+++ src/pages/Login.js
@@ -0,0 +1,27 @@
+const Login = () => {
+  return (
+    <div className="login-page">
+      <h1>Employee Polls</h1>
+      <img
+        className="login-page__cover"
+        src="https://www.applauz.me/hubfs/image%20%2822%29.png"
+      ></img>
+      <h2>Log In</h2>
+      <form className="form">
+        <label className="form__label">
+          User
+          <input className="form__input" placeholder="User" />
+        </label>
+        <label className="form__label">
+          Password
+          <input className="form__input" placeholder="Password" />
+        </label>
+        <button className="form__button" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
