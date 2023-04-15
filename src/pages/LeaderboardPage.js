06cf4df01029f0c24df7d723ce56ff14979b5e13
Felizardo Massimbe <felizardo.n.massimbe@aubay.pt>
2023-04-15 02:31:35 +0100

    feat: create leaderboard page layout

---
+++ src/pages/LeaderboardPage.js
@@ -0,0 +1,11 @@
+import Leaderboard from "../components/Leaderboard";
+
+const LeaderboardPage = () => {
+  return (
+    <div className="leaderboard-page">
+      <Leaderboard />
+    </div>
+  );
+};
+
+export default LeaderboardPage;
