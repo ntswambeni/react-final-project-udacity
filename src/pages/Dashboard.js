20ec9ca14a563faa8343dc14bc29497c63c70873
Felizardo Massimbe <felizardo.n.massimbe@aubay.pt>
2023-04-15 02:31:02 +0100

    feat: create dashboard page layout

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import PollList from "../components/PollList";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <PollList />
+      <PollList />
+    </div>
+  );
+};
+
+export default Dashboard;
