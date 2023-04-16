8f87cac6ea30fee9646ccbd2479fcb857cc93190
Felizardo Massimbe <felizardo.n.massimbe@aubay.pt>
2023-04-16 01:59:12 +0100

    feat: create app store and provide it

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { configureStore } from "@reduxjs/toolkit";
 
 import "./index.css";
 import App from "./App";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
+
+const store = configureStore({ reducer: rootReducer });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  </Provider>
 );
