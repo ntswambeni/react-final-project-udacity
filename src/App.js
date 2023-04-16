5daf8858e6f02b5d143a995ac1734b27297576cf
Felizardo Massimbe <felizardo.n.massimbe@aubay.pt>
2023-04-16 22:02:25 +0100

    feat: set protected routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import NewPoll from "./pages/NewPoll";
 import AuthedPage from "./pages/AuthedPage";
 import { handleInitialData } from "./actions/shared";
 import PollPage from "./pages/PollPage";
+import Protected from "./components/Protected";
+import authedUser from "./reducers/authedUser";
 
 function App(props) {
   useEffect(() => {
@@ -16,15 +18,54 @@ function App(props) {
   }, []);
   return (
     <Routes>
-      <Route path="/" element={<AuthedPage />}>
-        <Route path="" element={<Dashboard />} />
-        <Route path="leaderboard" element={<LeaderboardPage />} />
-        <Route path="new-poll" element={<NewPoll />} />
-        <Route path="poll/:id" element={<PollPage />} />
+      <Route
+        path="/"
+        element={
+          <Protected isSignedIn={authedUser}>
+            <AuthedPage />
+          </Protected>
+        }
+      >
+        <Route
+          path=""
+          element={
+            <Protected isSignedIn={authedUser}>
+              <Dashboard />
+            </Protected>
+          }
+        />
+        <Route
+          path="leaderboard"
+          element={
+            <Protected isSignedIn={authedUser}>
+              <LeaderboardPage />
+            </Protected>
+          }
+        />
+        <Route
+          path="new-poll"
+          element={
+            <Protected isSignedIn={authedUser}>
+              <NewPoll />
+            </Protected>
+          }
+        />
+        <Route
+          path="poll/:id"
+          element={
+            <Protected isSignedIn={authedUser}>
+              <PollPage />
+            </Protected>
+          }
+        />
       </Route>
       <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
+
+const mapStateToProps = ({ authedUser }) => ({
+  authedUser,
+});
 
 export default connect()(App);
