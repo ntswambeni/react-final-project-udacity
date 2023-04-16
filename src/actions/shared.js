3bf7269f6c7f3e508746b98cf274eccd78f100d0
Felizardo Massimbe <felizardo.n.massimbe@aubay.pt>
2023-04-16 03:48:09 +0100

    feath: add authed user reducer and actions

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,10 +1,13 @@
 import { getInitialData } from "../utils/api";
+import { setAuthedUser } from "./authedUser";
 import { receiveQuestions } from "./questions";
 import { receiveUsers } from "./users";
 
 export const handleInitialData = () => {
+  const AUTHED_ID = "sarahedo";
   return (dispatch) => {
     getInitialData().then(({ users, questions }) => {
+      dispatch(setAuthedUser(AUTHED_ID));
       dispatch(receiveUsers(users));
       dispatch(receiveQuestions(questions));
     });
