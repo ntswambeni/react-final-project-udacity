bb16c96ebae662e370dd04e5d42276e67729b67c
Felizardo Massimbe <felizardo.n.massimbe@aubay.pt>
2023-04-16 01:55:50 +0100

    fix: return default state

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -4,6 +4,8 @@ const questions = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_QUESTIONS:
       return { ...state, ...action.questions };
+    default:
+      return state;
   }
 };
 
